9c786b245a8e8ee21119502f0c13aa84702e9f9c
Baghdad <BaghdadHamdoud@gmail.com>
2021-07-10 16:14:01 +0100

    projects : review - hoverEffect

---
+++ src/features/projects/MySwiper/MySwiper.js
@@ -0,0 +1,76 @@
+import React, {useEffect} from "react";
+import './MySwiper.css'
+
+export const MySwiper = ({slides, class_container, class_wrapper, width, height}) => {
+
+    let position = width * (slides.length / 2 - 0.5)
+    let index = 0
+
+    useEffect(()=>{
+        const container = document.querySelector(`.${class_container}`)
+        container.style.width = String(2*width) + 'px'
+        container.style.height = String(height) + 'px'
+        const wrapper = document.querySelector(`.${class_wrapper}`)
+        wrapper.style.height = String(height) + 'px'
+        wrapper.style.transform = 'translateX('+ String(position) +'px)'
+        const slidesTags = document.querySelectorAll('.swiper_slide')
+        slidesTags[index].classList.add('currentSlide')
+        if(index + 1 <= slides.length - 1){
+            slidesTags[index + 1].classList.add('rightSlide')
+        }
+    },[])
+
+    const handeRight = () => {
+        if(index < slides.length - 1){
+            const slidesTags = document.querySelectorAll('.swiper_slide')
+            if(index - 1 >= 0){
+                slidesTags[index - 1].classList.remove('leftSlide')
+            }
+            slidesTags[index].classList.remove('currentSlide')
+            slidesTags[index].classList.add('leftSlide')
+            const wrapper = document.querySelector(`.${class_wrapper}`)
+            position -= width
+            wrapper.style.transform = 'translateX('+ String(position) +'px)'
+            index++
+            slidesTags[index].classList.remove('rightSlide')
+            slidesTags[index].classList.add('currentSlide')
+            if(index + 1 <= slides.length - 1){
+                slidesTags[index + 1].classList.add('rightSlide')
+            }
+        }
+    }
+
+    const handleLeft = () => {
+        if(index > 0){
+            const slidesTags = document.querySelectorAll('.swiper_slide')
+            if(index + 1 <= slides.length - 1){
+                slidesTags[index + 1].classList.remove('rightSlide')
+            }
+            slidesTags[index].classList.remove('currentSlide')
+            slidesTags[index].classList.add('rightSlide')
+            const wrapper = document.querySelector(`.${class_wrapper}`)
+            position += width
+            wrapper.style.transform = 'translateX('+ String(position) +'px)'
+            index--
+            slidesTags[index].classList.remove('leftSlide')
+            slidesTags[index].classList.add('currentSlide')
+            if(index - 1 >= 0){
+                slidesTags[index - 1].classList.add('leftSlide')
+            }
+        }
+    }
+
+    return(
+        <div className={`${class_container} swiper_container`}>
+            <div className={'swiper_arrow swiper_arrow_right'} onClick={handeRight}>&#62;</div>
+            <div className={'swiper_arrow swiper_arrow_left'} onClick={handleLeft}>&#60;</div>
+            <div className={`${class_wrapper} swiper_wrapper`}>
+                {slides.map(slide => (
+                    <div className={'swiper_slide'}
+                         style={{width: String(width) + 'px'}}
+                    >{slide}</div>
+                ))}
+            </div>
+        </div>
+    )
+}
--- src/features/projects/Projects.js
+++ src/features/projects/Projects.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React from "react"
 import './projects.css'
+import {MySwiper} from "./MySwiper/MySwiper";
 
 export const Projects = () => {
 
+    const slides = [
+        <img src={'./projects/messenger.jpg'} alt={'MessengerFacebookClone'} />,
+        <img src={'./projects/messenger.jpg'} alt={'Fruits'} />,
+        <img src={'./projects/messenger.jpg'} alt={'Khra'} />
+    ]
+
     return(
         <div className={'projects'}>
-            projects
+            <div className={'projects__title'}>
+                PROJECTS
+                <span className={'projects__title__line'}></span>
+            </div>
+            <MySwiper
+                slides={slides}
+                class_container={'projects__swiperContainer'}
+                class_wrapper={'projects__swiperWrapper'}
+                width={390}
+                height={300}
+                />
         </div>
     )
 }
